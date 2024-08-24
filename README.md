469a2fe01268042e0633d4c6d25b3afd67596840
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-08-24 22:01:45 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# eslint-config
+
+// WIP
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,37 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      curly: ["warn", "multi-line"],
+      eqeqeq: "error",
+      yoda: "warn",
+      "prefer-promise-reject-errors": ["error", { allowEmptyReject: true }],
+      "array-callback-return": ["error", { allowImplicit: true }],
+      "no-else-return": "warn",
+      "no-console": "error",
+      "no-constant-binary-expression": "error",
+      "no-useless-return": "warn",
+      "default-case-last": "error",
+      "no-promise-executor-return": "error",
+      "no-template-curly-in-string": "error",
+      "no-unreachable-loop": "error",
+      "no-duplicate-imports": "warn",
+      "no-multiple-empty-lines": "warn",
+      "no-label-var": "error",
+      "no-undef-init": "warn",
+      "no-unused-vars": [
+        "error",
+        {
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          ignoreRestSiblings: false,
+          vars: "all",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+];
